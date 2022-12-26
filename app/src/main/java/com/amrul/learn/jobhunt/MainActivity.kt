7a0211a643bdecc7520a08883716907bdde451b3
chairul <chairul.amri@ebconnect.com>
2022-12-26 19:35:20 +0700

    first project

---
+++ app/src/main/java/com/amrul/learn/jobhunt/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.amrul.learn.jobhunt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    @Suppress("DEPRECATION")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
+    }
+}
